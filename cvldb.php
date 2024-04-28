7a5b29aff7789a2c3aa53333b99e500303ac29e9
User <giogvalia62@gmail.com>
2024-04-28 20:15:44 +0400

    doctorsANDtestimonialsDONE

--- cvldb.php
+++ cvldb.php
@@ -2,19 +2,22 @@
 
 function slider () {
     $sliders = [
-        [
-            'h1' => 'Mico',
+        [   
+            'class'=>'carousel-item active',
+            'h1' => 'Gvalos',
             'span' => 'Hospital',
             'p' => 'when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to',
             'img' => 'images/slider-img.jpg',
         ],
         [
+            'class'=>'carousel-item',
             'h1' => 'Trico',
             'span' => 'Gospital',
             'p' => 'We will sell your heart after surgery and tell your family you died accidently',
             'img' => 'images/gospital.png',
         ],
         [
+            'class'=>'carousel-item',
             'h1' => 'Morgi',
             'span' => '100%',
             'p' => 'YOU WILL DIE 100%',
@@ -23,7 +26,10 @@ function slider () {
     ];
 
     foreach ($sliders as $slider)
-                echo '<div class="col-md-6">
+                echo '<div class="'. $slider['class'] .'">
+                <div class="container ">
+                  <div class="row">
+            <div class="col-md-6">
                 <div class="detail-box">
                   <div class="play_btn">
                     <button>
@@ -48,11 +54,146 @@ function slider () {
                 <div class="img-box">
                   <img src="'. $slider['img']   .'" alt="">
                 </div>
+              </div>
+              </div>
+              </div>
               </div>';
-}
+};
 
+function treatment() {
+    $treatments = [
+        [
+            'img' => 'images/t1.png',
+            'h4' => 'Nephrologist Care',
+            'p' => 'alteration in some form, by injected humour, or randomised words which dont look even slightly e sure there isnt anything',
+        ],
+        [
+            'img' => 'images/t2.png',
+            'h4' => 'Eye Care',
+            'p' => 'alteration in some form, by injected humour, or randomised words which dont look even slightly e sure there isnt anything',
+        ],
+        [
+            'img' => 'images/t3.png',
+            'h4' => 'Pediatrician Clinic',
+            'p' => 'alteration in some form, by injected humour, or randomised words which dont look even slightly e sure there isnt anything',
+        ],
+        [
+            'img' => 'images/t4.png',
+            'h4' => 'Parental Care',
+            'p' => 'alteration in some form, by injected humour, or randomised words which dont look even slightly e sure there isnt anything',
+        ],
+    ];
 
+    foreach ($treatments as $treatment) {
+        echo '<div class="col-md-6 col-lg-3">
+        <div class="box ">
+        <div class="img-box">
+              <img src="'. $treatment['img'] .'" alt="">
+            </div>
+            <div class="detail-box">
+              <h4>
+                '. $treatment['h4'] .'
+              </h4>
+              <p>
+                '. $treatment['p'] .'
+              </p>
+              <a href="">
+                Read More
+              </a>
+            </div>
+            </div>
+            </div>
+        ';
+    };
+};
 
+function doctors() {
+    $doctors = [
+        [
+            'img' => 'images/team1.jpg',
+            'h5' => 'Hennry',
+        ],
+        [
+            'img' => 'images/team2.jpg',
+            'h5' => 'Jenni',
+        ],
+        [
+            'img' => 'images/team3.jpg',
+            'h5' => 'Morco',
+        ],
+    ];
 
+    foreach ($doctors as $doctor) {
+        echo '<div class="item">
+        <div class="box">
+          <div class="img-box">
+            <img src="'. $doctor['img'] .'" alt="" />
+          </div>
+          <div class="detail-box">
+            <h5>
+              '. $doctor['h5'] .'
+            </h5>
+            <h6>
+              MBBS
+            </h6>
+            <div class="social_box">
+              <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>';
+    };
+};
 
+function testimonials() {
+    $testimonials = [
+        [
+            'class' => 'carousel-item active',
+            'h5' => 'Morijorch',
+            'p' => 'editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various',
+        ],
+        [
+            'class' => 'carousel-item',
+            'h5' => 'Rochak',
+            'p' => 'Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsu will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy.',
+        ],
+        [
+            'class' => 'carousel-item',
+            'h5' => 'Brad Johns',
+            'p' => 'Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy, editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various',
+        ],
+    ];
+
+    foreach ($testimonials as $testimonial) {
+        echo '<div class="'. $testimonial['class'] .'">
+        <div class="box">
+          <div class="client_info">
+            <div class="client_name">
+              <h5>
+                '. $testimonial['h5'] .'
+              </h5>
+              <h6>
+                Default model text
+              </h6>
+            </div>
+            <i class="fa fa-quote-left" aria-hidden="true"></i>
+          </div>
+          <p>
+            '. $testimonial['p'] .'
+          </p>
+        </div>
+      </div>';
+    };
+};
 ?>
--- index.php
+++ index.php
@@ -127,6 +127,8 @@
       </div>
     </header>
     <!-- end header section -->
+
+
     <!-- slider section -->
     <section class="slider_section ">
       <div class="dot_design">
@@ -134,16 +136,11 @@
       </div>
       <div id="customCarousel1" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item-active">
-            <div class="container ">
-              <div class="row">
+          
                 <?php 
                 slider ()
                 ?>
-              </div>
-            </div>
-          </div>
-        
+             
         </div>
         <div class="carousel_btn-box">
           <a class="carousel-control-prev" href="#customCarousel1" role="button" data-slide="prev">
@@ -156,7 +153,6 @@
           </a>
         </div>
       </div>
-
     </section>
     <!-- end slider section -->
   </div>
@@ -271,78 +267,7 @@
         </h2>
       </div>
       <div class="row">
-        <div class="col-md-6 col-lg-3">
-          <div class="box ">
-            <div class="img-box">
-              <img src="images/t1.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h4>
-                Nephrologist Care
-              </h4>
-              <p>
-                alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="box ">
-            <div class="img-box">
-              <img src="images/t2.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h4>
-                Eye Care
-              </h4>
-              <p>
-                alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="box ">
-            <div class="img-box">
-              <img src="images/t3.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h4>
-                Pediatrician Clinic
-              </h4>
-              <p>
-                alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="box ">
-            <div class="img-box">
-              <img src="images/t4.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h4>
-                Parental Care
-              </h4>
-              <p>
-                alteration in some form, by injected humour, or randomised words which don't look even slightly e sure there isn't anything
-              </p>
-              <a href="">
-                Read More
-              </a>
-            </div>
-          </div>
-        </div>
+        <?php treatment() ?>
       </div>
     </div>
   </section>
@@ -360,93 +285,7 @@
       </div>
       <div class="carousel-wrap ">
         <div class="owl-carousel team_carousel">
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/team1.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Hennry
-                </h5>
-                <h6>
-                  MBBS
-                </h6>
-                <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/team2.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Jenni
-                </h5>
-                <h6>
-                  MBBS
-                </h6>
-                <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/team3.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Morco
-                </h5>
-                <h6>
-                  MBBS
-                </h6>
-                <div class="social_box">
-                  <a href="">
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <?php doctors() ?>
         </div>
       </div>
     </div>
@@ -467,60 +306,7 @@
     <div class="container px-0">
       <div id="customCarousel2" class="carousel  carousel-fade" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="box">
-              <div class="client_info">
-                <div class="client_name">
-                  <h5>
-                    Morijorch
-                  </h5>
-                  <h6>
-                    Default model text
-                  </h6>
-                </div>
-                <i class="fa fa-quote-left" aria-hidden="true"></i>
-              </div>
-              <p>
-                editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="box">
-              <div class="client_info">
-                <div class="client_name">
-                  <h5>
-                    Rochak
-                  </h5>
-                  <h6>
-                    Default model text
-                  </h6>
-                </div>
-                <i class="fa fa-quote-left" aria-hidden="true"></i>
-              </div>
-              <p>
-                Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="box">
-              <div class="client_info">
-                <div class="client_name">
-                  <h5>
-                    Brad Johns
-                  </h5>
-                  <h6>
-                    Default model text
-                  </h6>
-                </div>
-                <i class="fa fa-quote-left" aria-hidden="true"></i>
-              </div>
-              <p>
-                Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy, editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-              </p>
-            </div>
-          </div>
+          <?php testimonials() ?>
         </div>
         <div class="carousel_btn-box">
           <a class="carousel-control-prev" href="#customCarousel2" role="button" data-slide="prev">
